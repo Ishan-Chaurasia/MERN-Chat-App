e039a4a30f59ea7fc52f9adef27f59e467199fb1
Icbeas5 <ishanchaurasia218@gmail.com>
2026-07-23 04:27:41 +0530

    sound fix

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,22 +1,27 @@
 import { useEffect } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
-import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages } = useConversation();
+  const { setMessages } = useConversation();
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
       newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play();
-      setMessages([...messages, newMessage]);
+      
+      // 1. No import needed! Just use the string directly.
+      const sound = new Audio("/notification.mp3");
+      sound.play().catch((err) => console.log("Sound blocked by browser:", err));
+      
+      // 2. Safely get the previous messages without relying on the dependency array
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
     });
 
-    return () => socket.off("newMessage");
-  }, [socket, setMessages, messages]);
+    // 3. Added optional chaining (?) just to be safe
+    return () => socket?.off("newMessage");
+    
+  }, [socket, setMessages]); // 4. 'messages' is strictly removed from here!
 };
 
 export default useListenMessages;
